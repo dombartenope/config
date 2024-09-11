b0e7dc36e2e517e482d9e2cd75779236e911c12d
Dominick Bartenope <dominick@onesignal.com>
2024-09-11 10:46:19 -0400

    Requested added gitignore plugin to vim config

---
+++ nvim/lua/dom/plugins/gitignore.lua
@@ -0,0 +1,10 @@
+return {
+    "wintermute-cell/gitignore.nvim",
+    config = function()
+        require('gitignore')
+    end,
+}
+
+-- TO USE: 
+-- :Gitignore [path]
+-- https://github.com/wintermute-cell/gitignore.nvim
